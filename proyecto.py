61931b3f5279325a0c0c22421f7fdad0221c4f50
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-08-21 23:13:56 -0600

    menu inicio

---
+++ proyecto.py
@@ -0,0 +1,8 @@
+class Menus:
+    def menu(self):
+        print("\nBIENVENIDO AL MENÚ DE BEAUTIFUL ROSE")
+        print("[1] Registro de productos")
+        print("[2] Registro de empleados")
+        print("[3] Compra de productos")
+        print("[4] venta de productos")
+        print("[5] Salir del menú")
