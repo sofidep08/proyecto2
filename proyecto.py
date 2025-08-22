daebef6977bff56f8ce9f37eb8b0cf53121566c8
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-08-21 23:44:16 -0600

    productos

--- proyecto.py
+++ proyecto.py
@@ -6,3 +6,34 @@ class Menus:
         print("[3] Compra de productos")
         print("[4] venta de productos")
         print("[5] Salir del menú")
+
+class BaseDatos:
+    productos = {}
+    categorias = {}
+    clientes = {}
+    empleados = {}
+    proveedores = {}
+    ventas = {}
+    compras = {}
+    detalles_venta = {}
+    detalles_compra = {}
+
+class Producto:
+    def __init__(self, id_producto, nombre, id_categoria, precio, stock):
+        self.id_producto = id_producto
+        self.nombre = nombre
+        self.id_categoria = id_categoria
+        self.precio = precio
+        self.stock = stock
+        self.total_compras = 0
+        self.total_ventas = 0
+
+        BaseDatos.productos[self.id_producto] = self
+
+    def actualizar_stock(self, cantidad, operacion):
+        if operacion == 'compra':
+            self.stock += cantidad
+            self.total_compras += cantidad
+        elif operacion == 'venta':
+            self.stock -= cantidad
+            self.total_ventas += cantidad
