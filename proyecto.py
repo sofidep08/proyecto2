0365d73b6aa5426e3b94cd6431df03babc0bc767
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-08-24 20:51:41 -0600

    compraa

--- proyecto.py
+++ proyecto.py
@@ -93,6 +93,19 @@ class Compra:
     def calcular_total(self):
         return sum(BaseDatos.detalles_compra[d].subtotal() for d in self.detalles_ids)
 
+
+class DetalleCompra:
+    def __init__(self, id_detalle_compra, id_compra, id_producto, cantidad, precio_compra, fecha_caducidad):
+        self.id_detalle_compra = id_detalle_compra
+        self.id_compra = id_compra
+        self.id_producto = id_producto
+        self.cantidad = cantidad
+        self.precio_compra = precio_compra
+        self.fecha_caducidad = fecha_caducidad
+
+    def subtotal(self):
+        return self.cantidad * self.precio_compra
+
 class Venta:
     def __init__(self, id_venta, fecha, nit_cliente, id_empleado):
         self.id_venta = id_venta
